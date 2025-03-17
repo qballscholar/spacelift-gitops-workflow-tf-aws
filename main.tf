9fda6dd64d441cd0781d73b2a8cb6778e2a24a24
Ralph Quick <qballscholar@gmail.com>
2025-03-17 15:06:52 -0400

    Add initial Terraform configuration

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-08b5b3a93ed654d19"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "Spacelift-Demo"
+    Environment = "Development"
+  }
+}
