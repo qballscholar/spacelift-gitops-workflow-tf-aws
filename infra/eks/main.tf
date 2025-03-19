3c13fe65b5ff0a0cf4e518c28e6847314b9661fb
[Ralph Quick] <[qballscholar@gmail.com]>
2025-03-19 13:50:35 -0400

    revise structure and config

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "your-terraform-state-bucket"
+    bucket = "tf-state-bucket-spacelift"
     key    = "spacelift/terraform.tfstate"
     region = "us-east-1"
   }
