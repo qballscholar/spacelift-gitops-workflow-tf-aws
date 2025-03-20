b8e07b999e489c4ab340126f763d8c4d85e72657
[Ralph Quick] <[qballscholar@gmail.com]>
2025-03-20 01:33:37 -0400

    removed revisions from tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
 # This file is for documentation only
 # Each stack (vpc, eks) has its own Terraform configuration
 terraform {
-  required_version = "~> 1.5.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
